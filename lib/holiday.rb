13b9fb2039611d65a29568ef42c7f8ea274eaabd
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-16 03:45:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  array = holiday_hash[:winter].map do |holiday, supply|
+    supply
+  end
+array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
